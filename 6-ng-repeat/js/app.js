3554da50473c111f84e3b11d66206d5485ca250c
Dixit Patel <talkwithdixit@gmail.com>
2016-05-10 11:05:32 -0400

    Added ng-show and ng-hide functionality

--- 6-ng-repeat/js/app.js
+++ 6-ng-repeat/js/app.js
@@ -1,8 +1,14 @@
-var items = [1,2,3,4,5,6,7,8];
+var items = [1, '2', 3, '4', 5, '6', 7, 'Patel'];
+
 var val = ' and it\' index';
-items.forEach(function(item, index) {
-  console.log(item, val, index);
+items.forEach(function(item, index, items) {
+    console.log(item, val, index);
 });
 
+
+// for(var i = 0; i < items.length; ++i){
+//   console.log(items[i] + ' and it\'s index is: ' + i);
+// }
+// setter for the main module
 angular
-  .module('app', []);
+    .module('app', []);
