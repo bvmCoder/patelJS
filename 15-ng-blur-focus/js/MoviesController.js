f8538c8bb82eb9d626d598f15fafed3555a4ed58
Dixit Patel <talkwithdixit@gmail.com>
2016-05-10 13:39:40 -0400

    Added focus and blur js event to the Demo File

--- 15-ng-blur-focus/js/MoviesController.js
+++ 15-ng-blur-focus/js/MoviesController.js
@@ -1,38 +1,40 @@
-function MoviesController() {
-  var self = this;
+var MoviesController = function MoviesController() {
+    var self = this;
+    self.newTitle = self.newRelease = '';
 
-  this.newTitle = '';
-  this.newRelease = '';
-  this.onFocus = function onFocus() {
-    console.log('Focus!');
-  };
-  this.onBlur = function onBlur() {
-    console.log('Blur!');
-  };
-  this.addMovie = function () {
-    this.favorites.unshift({
-      title: this.newTitle,
-      year: this.newRelease
-    })
-  };
-  this.favorites = [{
-    title: 'The Shawshank Redemption',
-    year: '1994'
-  }, {
-    title: 'Inception',
-    year: '2010'
-  },{
-    title: 'The Matrix',
-    year: '1999'
-  }, {
-    title: 'Saving Private Ryan',
-    year: '1998'
-  },{
-    title: 'The Aviator',
-    year: '2004'
-  }];
-}
+    //self.newTitle = '';
+    //self.newRelease = '';
+    self.onFocus = function onFocus() {
+        console.log('Focus!');
+    };
+    self.onBlur = function onBlur() {
+        console.log('Blur!');
+    };
+    self.addMovie = function addMovie() {
+        self.favorites.unshift({
+            title: self.newTitle,
+            year: self.newRelease
+        })
+        self.newTitle = self.newRelease = '';
+    };
+    self.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994'
+    }, {
+        title: 'Inception',
+        year: '2010'
+    }, {
+        title: 'The Matrix',
+        year: '1999'
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    }, {
+        title: 'The Aviator',
+        year: '2004'
+    }];
+};
 
 angular
-  .module('app')
-  .controller('MoviesController', MoviesController);
+    .module('app')
+    .controller('MoviesController', MoviesController);
