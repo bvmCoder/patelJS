25407adabf7d592be28cef32a2136e65b67e92ea
Dixit Patel <talkwithdixit@gmail.com>
2016-05-10 11:12:51 -0400

    Added ng-if functionality to the page

--- 8-ng-if/js/MoviesController.js
+++ 8-ng-if/js/MoviesController.js
@@ -1,28 +1,28 @@
 function MoviesController() {
-  var self = this;
-  self.favorites = [{
-    title: 'The Shawshank Redemption',
-    year: '1994',
-    popular: true
-  }, {
-    title: 'Inception',
-    year: '2010',
-    popular: false
-  }, {
-    title: 'The Matrix',
-    year: '1999',
-    popular: true
-  }, {
-    title: 'Saving Private Ryan',
-    year: '1998',
-    popular: true
-  }, {
-    title: 'The Aviator',
-    year: '2004',
-    popular: false
-  }];
+    var self = this;
+    self.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994',
+        popular: true
+    }, {
+        title: 'Inception',
+        year: '2010',
+        popular: false
+    }, {
+        title: 'The Matrix',
+        year: '1999',
+        popular: true
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998',
+        popular: true
+    }, {
+        title: 'The Aviator',
+        year: '2004',
+        popular: false
+    }];
 }
 
 angular
-  .module('app')
-  .controller('MoviesController', MoviesController);
+    .module('app')
+    .controller('MoviesController', MoviesController);
