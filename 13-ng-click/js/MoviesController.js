18c52ac107d847f3d5042fb6d9d247c92ba999ad
Dixit Patel <talkwithdixit@gmail.com>
2016-05-10 13:14:03 -0400

    Added ng-click directive functionality to the Demo file;

--- 13-ng-click/js/MoviesController.js
+++ 13-ng-click/js/MoviesController.js
@@ -1,33 +1,33 @@
-function MoviesController() {
-  //var self = this;
-  this.likesList = [];
+var MoviesController = function MoviesController() {
+    var self = this;
+    self.likesList = [];
 
-  this.unlike = function (index) {
-    this.likesList.splice(index, 1);
-  };
+    self.unlike = function unlike(index) {
+        self.likesList.splice(index, 1);
+    };
 
-  this.addToLikes = function (movie) {
-    this.likesList.unshift(movie);
-  };
+    self.addToLikes = function addToLikes(movie) {
+        self.likesList.push(movie);
+    };
 
-  this.favorites = [{
-    title: 'The Shawshank Redemption',
-    year: '1994'
-  }, {
-    title: 'Inception',
-    year: '2010'
-  },{
-    title: 'The Matrix',
-    year: '1999'
-  }, {
-    title: 'Saving Private Ryan',
-    year: '1998'
-  }, {
-    title: 'The Aviator',
-    year: '2004'
-  }];
-}
+    self.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994'
+    }, {
+        title: 'Inception',
+        year: '2010'
+    }, {
+        title: 'The Matrix',
+        year: '1999'
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    }, {
+        title: 'The Aviator',
+        year: '2004'
+    }];
+};
 
 angular
-  .module('app')
-  .controller('MoviesController', MoviesController);
+    .module('app')
+    .controller('MoviesController', MoviesController);
