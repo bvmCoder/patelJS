f2cc9ceeeeac049c6cac083006030f98d6e6b1fa
Dixit Patel <talkwithdixit@gmail.com>
2016-05-10 12:07:43 -0400

    Added ng-class functionality for the demo tutorial

--- 10-ng-class/js/MoviesController.js
+++ 10-ng-class/js/MoviesController.js
@@ -1,24 +1,24 @@
 function MoviesController() {
-  var self = this;
+    var self = this;
 
-  self.favorites = [{
-    title: 'The Shawshank Redemption',
-    year: '1994'
-  }, {
-    title: 'Inception',
-    year: '2010'
-  },{
-    title: 'The Matrix',
-    year: '1999'
-  },{
-    title: 'Saving Private Ryan',
-    year: '1998'
-  },{
-    title: 'The Aviator',
-    year: '2004'
-  }];
+    self.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994'
+    }, {
+        title: 'Inception',
+        year: '2010'
+    }, {
+        title: 'The Matrix',
+        year: '1999'
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    }, {
+        title: 'The Aviator',
+        year: '2004'
+    }];
 }
 
 angular
-  .module('app')
-  .controller('MoviesController', MoviesController);
+    .module('app')
+    .controller('MoviesController', MoviesController);
