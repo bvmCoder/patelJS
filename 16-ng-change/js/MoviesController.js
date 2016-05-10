a69ef5e10ba8fc69a42dee4cbad05bd361da1e8d
Dixit Patel <talkwithdixit@gmail.com>
2016-05-10 00:22:37 -0400

    Added expression binidngs and ng-change directive

--- 16-ng-change/js/MoviesController.js
+++ 16-ng-change/js/MoviesController.js
@@ -1,35 +1,35 @@
 function MoviesController() {
-  var self = this;
+    var self = this;
 
-  this.newTitle = '';
-  this.newRelease = '';
-  this.onChange = function onChange() {
-    console.log('Change!', this.newTitle);
-  };
-  this.addMovie = function addMovie() {
-    this.favorites.unshift({
-      title: this.newTitle,
-      year: this.newRelease
-    })
-  };
-  this.favorites = [{
-    title: 'The Shawshank Redemption',
-    year: '1994'
-  }, {
-    title: 'Inception',
-    year: '2010'
-  },{
-    title: 'The Matrix',
-    year: '1999'
-  }, {
-    title: 'Saving Private Ryan',
-    year: '1998'
-  }, {
-    title: 'The Aviator',
-    year: '2004'
-  }];
+    this.newTitle = '';
+    this.newRelease = '';
+    this.onChange = function onChange() {
+        console.log('Change!', this.newTitle);
+    };
+    this.addMovie = function addMovie() {
+        this.favorites.unshift({
+            title: this.newTitle,
+            year: this.newRelease
+        });
+    };
+    this.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994'
+    }, {
+        title: 'Inception',
+        year: '2010'
+    }, {
+        title: 'The Matrix',
+        year: '1999'
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    }, {
+        title: 'The Aviator',
+        year: '2004'
+    }];
 }
 
 angular
-  .module('app')
-  .controller('MoviesController', MoviesController);
+    .module('app')
+    .controller('MoviesController', MoviesController);
