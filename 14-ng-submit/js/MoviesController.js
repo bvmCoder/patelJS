295688acfe9c0b3560ee04ce194349f1d10fef5a
Dixit Patel <talkwithdixit@gmail.com>
2016-05-10 13:33:46 -0400

    Added orderBy filter to the ngRepeat directive

--- 14-ng-submit/js/MoviesController.js
+++ 14-ng-submit/js/MoviesController.js
@@ -1,37 +1,37 @@
 var MoviesController = function MoviesController() {
-  var self = this;
+    var self = this;
 
-  this.newTitle = ''; // empty string initially
-  this.newRelease = ''; // empty string initially
+    self.newTitle = ''; // empty string initially
+    self.newRelease = ''; // empty string initially
 
-  this.addMovie = function addMovie() {
-    this.favorites.unshift({
-      title: this.newTitle,
-      year: this.newRelease
-    });
-    this.newTitle = this.newRelease = '';
+    self.addMovie = function addMovie() {
+        self.favorites.unshift({
+            title: self.newTitle,
+            year: self.newRelease
+        });
+        self.newTitle = self.newRelease = '';
 
-  };
-  this.favorites = [{
-    title: 'The Shawshank Redemption',
-    year: '1994'
-  }, {
-    title: 'Inception',
-    year: '2010'
-  },{
-    title: 'The Matrix',
-    year: '1999'
-  }, {
-    title: 'Saving Private Ryan',
-    year: '1998'
-  }, {
-    title: 'The Aviator',
-    year: '2004'
-  }];
+    };
+    self.favorites = [{
+        title: 'The Shawshank Redemption',
+        year: '1994'
+    }, {
+        title: 'Inception',
+        year: '2010'
+    }, {
+        title: 'The Matrix',
+        year: '1999'
+    }, {
+        title: 'Saving Private Ryan',
+        year: '1998'
+    }, {
+        title: 'The Aviator',
+        year: '2004'
+    }];
 };
 
 angular
-  .module('app')
-  .controller('MoviesController', MoviesController);
+    .module('app')
+    .controller('MoviesController', MoviesController);
 
-  //this.newTitle = this.newRelease = '';
+//this.newTitle = this.newRelease = '';
