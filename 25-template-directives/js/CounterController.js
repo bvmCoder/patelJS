2e7c0ca5739cefd8e218b1816c419859b1da46e8
Dixit Patel <talkwithdixit@gmail.com>
2016-05-23 01:03:08 -0400

    Added two more directive stretch and counter directive

---
+++ 25-template-directives/js/CounterController.js
@@ -0,0 +1,14 @@
+var CounterController = function CounterController() {
+    var self = this;
+    self.count = 1;
+    self.increment = function increment() {
+        self.count++;
+    };
+    self.decrement = function decrement() {
+        self.count--;
+    };
+};
+
+angular
+    .module('app')
+    .controller('CounterController', CounterController);
