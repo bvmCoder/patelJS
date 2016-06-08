c5d44002f0ecb7208998f1b74a1710623591dd70
Dixit Patel <talkwithdixit@gmail.com>
2016-06-08 04:22:30 -0400

    Added mgCounter Directive into this Angular Demo Project

---
+++ 25-template-directives/js/mgCounter.directive.js
@@ -0,0 +1,23 @@
+var mgCounter = function mgCounter() {
+
+    return {
+        restrict: 'E',
+        //scope: {},
+        controller: 'CounterController as counter',
+        template: `
+          <div class="counter">
+            <input type="text" ng-model="counter.count" />
+            <button type="button" ng-click="counter.decrement();">
+              -
+            </button>
+            <button type="button" ng-click="counter.increment();">
+              +
+            </button>
+          </div>
+        `
+    };
+};
+
+angular
+    .module('app')
+    .directive('mgCounter', mgCounter);
