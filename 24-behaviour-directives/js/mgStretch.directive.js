1499ff989d269c97f7c831a7c2c49a0a9915510c
Dixit Patel <talkwithdixit@gmail.com>
2016-06-08 04:05:59 -0400

    Added mg-stretch directive as a custom directive on the input element

---
+++ 24-behaviour-directives/js/mgStretch.directive.js
@@ -0,0 +1,29 @@
+var mgStretch = function mgStretch() {
+
+    return {
+        restrict: 'A',
+        link: function link($scope, $element, $attrs) {
+            console.dir($element);
+
+            var element = $element[0];
+
+            console.dir(element);
+            element.addEventListener('focus', function() {
+                this.style.width = 250 + 'px';
+                this.style.padding = '4px 2px';
+                //this.style.outline = 'red auto 2px';
+                console.dir(this.style);
+            }, false);
+            element.addEventListener('blur', function() {
+                this.style.width = 120 + 'px';
+                this.style.padding = '0px';
+                //this.style.outline = 'none';
+            }, false);
+        }
+    };
+
+};
+
+angular
+    .module('app')
+    .directive('mgStretch', mgStretch);
