1a9209c37f47aaca78105e07e59d8b69c7859351
Dixit Patel <talkwithdixit@gmail.com>
2016-10-02 23:08:43 -0400

    Added mulitSlot Transclusion

---
+++ multislotTransclusion1-5X/js/directives/ContactCard.js
@@ -0,0 +1,22 @@
+(function() {
+    'use strict';
+
+    var ContactCard = function ContactCard() {
+        return {
+            transclude: {
+                name: 'h1',
+                desc: 'p'
+            },
+            template: `
+			<div>
+				<span ng-transclude="name"></span>
+				<div ng-transclude="desc">No description</div>
+			</div>
+		`
+        };
+    };
+
+    angular
+        .module('app')
+        .directive('contactCard', ContactCard);
+})();
