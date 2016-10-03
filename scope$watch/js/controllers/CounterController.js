b5415ad567cc1988f01c4df1b2f786917393603c
Dixit Patel <talkwithdixit@gmail.com>
2016-10-02 21:38:27 -0400

    Added .watch method

--- scope$watch/js/controllers/CounterController.js
+++ scope$watch/js/controllers/CounterController.js
@@ -3,29 +3,26 @@
 
     var CounterController = function CounterController($scope) {
         var self = this;
-
         self.count = 0;
         self.countList = [];
-        self.increment = function increment(){
-           self.count++;
-           self.countList.push({ id: self.count });
-        };
 
-        self.decrement = function decrement(){
-           self.count--;
-           self.countList.pop({ id: self.count });
+        self.increment = function increment() {
+            self.count++;
+            self.countList.unshift({ id: self.count });
         };
-
-        $scope.$watchCollection(angular.bind(self, function () {
+        self.decrement = function decrement() {
+            self.count--;
+            self.countList.unshift({ id: self.count });
+        };
+        $scope.$watchCollection(angular.bind(self, function() {
             return self.countList;
         }), function(newValue, oldValue) {
-            if(newValue === oldValue) return ;
-
+            if (newValue === oldValue) {
+                return;
+            }
             console.log(newValue, oldValue);
         });
     };
-
-    CounterController.$inject = ['$scope'];
 
     angular
         .module('app')
